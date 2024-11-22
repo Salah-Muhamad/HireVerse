7ba84bccf87eb28ac16605b43f92092bac070d42
Salah Muhamad <144562089+Salah-Muhamad@users.noreply.github.com>
2024-11-22 22:28:22 +0200

    add main fonts

--- src/Component/Home/Home.jsx
+++ src/Component/Home/Home.jsx
@@ -1,7 +1,9 @@
-import React from 'react'
-import styles from './Home.module.css';
+import React from "react";
+
 export default function Home() {
   return (
-    <div className='text-primary'>Home</div>
-  )
+    <>
+      
+    </>
+  );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,28 @@
 // tailwind.config.js
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    fontFamily:{
-      
+    fontFamily: {
+      bai_jamjuree: ["Bai Jamjuree", "sans-serif"],
+      sf_pro_display: ["SF Pro Display", "sans-serif"],
+      sf_pro_text: ["SF Pro Text", "sans-serif"],
     },
     extend: {
-      colors : {
-        primary : "#172554" , 
-      } 
+      fontWeight: {
+        light: "300",
+        normal: "400",
+        medium: "500",
+        semi_bold: "600",
+        bold: "700",
+        extra_bold: "800",
+        black: "900",
+      },
+      colors: {
+        primary: "#172554",
+        secondary: "#EDF3FF",
+        orange: "#F4A120"
+      },
     },
-    
   },
   plugins: [],
 };
