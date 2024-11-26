81efbdcc062755fa0542e3606dcea7e677795f00
shadaAhmed47 <115115874+shadaAhmed47@users.noreply.github.com>
2024-11-27 00:22:53 +0200

    end about

--- src/Component/AboutImageCard/AboutImageCard.jsx
+++ src/Component/AboutImageCard/AboutImageCard.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
-import bashaelbalad from '../../assets/Images/bashaelbalad.svg'
-export default function AboutImageCard() {
+
+export default function AboutImageCard({image ,name ,pos}) {
   return (
     <div className='mt-11 mb-11'>
       <div className="card rounded-xl w-72	 h-96">
-        <img src={bashaelbalad} className='w-full h-full'  />
-        <p className='font-sf_pro_text ms-3 font-semibold	'>Salah Muhamed</p>
-        <p className='ms-3'>Front-End Developer</p>
+        <img src={image} className='w-full h-full'  />
+        <p className='font-sf_pro_text ms-3 font-semibold'>{name}</p>
+        <p className='font-sf_pro_text text-[#838383] text-[.983rem] ms-3'>{pos}</p>
       </div>
     </div>
   )
