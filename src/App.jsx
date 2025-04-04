cba81718e14fff5b84d147f6f0610904591a8b05
Salah Muhamad <144562089+Salah-Muhamad@users.noreply.github.com>
2025-04-04 04:36:14 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -18,29 +18,10 @@ import ForgotPassword from "./Component/ForgotPassword/ForgotPassword";
 import VerifyEmail from "./Component/VerifyEmail/VerifyEmail";
 import ApplicantJobs from "./Component/ApplicantJobs/ApplicantJobs";
 import ApplyJob from "./Component/ApplyJob/ApplyJob";
+import ProfileSettings from "./Component/ProfileSettings/ProfileSettings";
 import { Toaster } from "react-hot-toast";
-import JobDetails from './Component/JobDetails/JobDetails'
-import { useState } from 'react';
-import Home from './Component/Home/Home';
-import UserContextProvider from './Context/UserContext.jsx';
-import CompanyDetails from './Component/CompanyDetails/CompanyDetails';
-import About from './Component/About/About';
-import Register from './Component/Register/Register';
-import Login from './Component/Login/Login';
-import Companies from './Component/Companies/Companies';
-import JobsPage from './Component/JobsPage/JobsPage';
-import NotFound from './Component/NotFound/NotFound';
-import Layout from './Component/Layout/Layout';
-import SignUpApplicant from './Component/SignUpApplicant/SignUpApplicant';
-import SignUpCompany1 from './Component/SignUpCompany1/SignUpCompany1';
-import SignUpCompany2 from './Component/SignUpCompany2/SignUpCompany2';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import ForgotPassword from './Component/ForgotPassword/ForgotPassword';
-import VerifyEmail from './Component/VerifyEmail/VerifyEmail';
-import ProfileSettings from './Component/ProfileSettings/ProfileSettings';
 
-import ApplicantJobs from './Component/ApplicantJobs/ApplicantJobs';
-import ApplyJob from './Component/ApplyJob/ApplyJob';
+
 
 
 
